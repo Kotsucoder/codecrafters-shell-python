c50c193cd53d1f78d33c5f6d8d72e0b2500b482e
KotsuCoder <whoami@kotsu.red>
2026-01-06 18:21:15 -0700

    Implement type

--- app/main.py
+++ app/main.py
@@ -8,12 +8,18 @@ def main():
         request = input().split()
         command = request[0]
         args = request[1:]
+        builtins = ["echo", "exit", "type"]
         match command:
             case "exit":
                 break
             case "echo":
                 string = " ".join(args)
                 sys.stdout.write(string + "\n")
+            case "type":
+                if args[0] in builtins:
+                    sys.stdout.write(f"{args[0]}: is a shell builtin\n")
+                else:
+                    sys.stdout.write(f"{args[0]}: not found\n")
             case _:
                 sys.stdout.write(f"{command}: command not found\n")
 
