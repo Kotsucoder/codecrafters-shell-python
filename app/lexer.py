15f70463d06733810f134646b1da2fe15edba39f
KotsuCoder <whoami@kotsu.red>
2026-01-11 00:29:45 -0700

    Escaping now works in double quotes

--- app/lexer.py
+++ app/lexer.py
@@ -21,7 +21,7 @@ def command_lexer(usrinput:str) -> List[SemanticToken]:
         escaped = False
 
         for char in usrinput:
-            if char == "\\" and not in_quotes:
+            if char == "\\" and not in_quotes and token_has_quotes != QuoteState.SINGLE:
                 escaped = True
             elif escaped:
                 current_word = current_word + char
